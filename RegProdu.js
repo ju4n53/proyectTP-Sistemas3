a3a84de0859dceaa0ef8f9895ae8511f77db7ec0
Juan <juansegutierrez7@gmail.com>
2023-05-24 04:40:08 -0300

    cambios header img y button salir

--- RegProdu.js
+++ RegProdu.js
@@ -1,37 +1,41 @@
-let codigo=[];
-let producto=[];
-let precio=[];
-let i=0;
-function carga1()
-{
-	const _txtCodigo =document.getElementById("txtCodigo");
-	const _txtProducto =document.getElementById("txtProducto");
-	const _txtPrecio =document.getElementById("txtPrecio");
-		codigo[i]=_txtCodigo.value;
-		producto[i]=_txtProducto.value;
-		precio[i]=parseInt(_txtPrecio.value);
-		i++;
-		     const _td0 = document.createElement("td");
-            _td0.innerHTML = _txtCodigo.value;
+let contador = 100;
+let codigo = [];
+let producto = [];
+let precio = [];
+let i = 0;
+function carga1() {
+	//const _txtCodigo = document.getElementById("txtCodigo");
+	const _txtProducto = document.getElementById("txtProducto");
+	const _txtPrecio = document.getElementById("txtPrecio");
 
-            const _td1 = document.createElement("td");
-            _td1.innerHTML = _txtProducto.value;
+	//codigo[i] = _txtCodigo.value;
+	producto[i] = _txtProducto.value;
+	precio[i] = parseInt(_txtPrecio.value);
+	i++;
+	contador = contador + 1;
 
-            const _td2 = document.createElement("td");
-            _td2.innerHTML = _txtPrecio.value;
+	const _td0 = document.createElement("td");
+	_td0.innerHTML = contador;
 
-            const _tr = document.createElement("tr");
-            _tr.appendChild(_td0);
-            _tr.appendChild(_td1);
-            _tr.appendChild(_td2);
+	const _td1 = document.createElement("td");
+	_td1.innerHTML = _txtProducto.value;
 
-            const _tbody = document.querySelector("tbody");
-            _tbody.appendChild(_tr);
+	const _td2 = document.createElement("td");
+	_td2.innerHTML = _txtPrecio.value;
 
-		_txtCodigo.value="";
-		_txtProducto.value="";
-		_txtPrecio.value="";
-		_txtCodigo.focus();
+	const _tr = document.createElement("tr");
+	_tr.appendChild(_td0);
+	_tr.appendChild(_td1);
+	_tr.appendChild(_td2);
+
+	const _tbody = document.querySelector("tbody");
+	_tbody.appendChild(_tr);
+
+
+	//_txtCodigo.value = "";
+	_txtProducto.value = "";
+	_txtPrecio.value = "";
+	_txtProducto.focus();
 
 
 }
--- RegProdu2.js
+++ RegProdu2.js
@@ -1,37 +1,40 @@
-let codigo2=[];
-let bebida=[];
-let precio2=[];
-let e=0;
-function carga2()
-{
-	const _txtCodigo2 =document.getElementById("txtCodigo2");
-	const _txtBebida =document.getElementById("txtBebida");
-	const _txtPrecio2 =document.getElementById("txtPrecio2");
-		codigo2[e]=_txtCodigo2.value;
-		bebida[e]=_txtBebida.value;
-		precio2[e]=parseInt(_txtPrecio2.value);
-		e++;
-		     const _td0 = document.createElement("td");
-            _td0.innerHTML = _txtCodigo2.value;
+let codigo2 = [];
+let bebida = [];
+let precio2 = [];
+let e = 0;
+let contador2 = 300;
+function carga2() {
+	//const _txtCodigo2 = document.getElementById("txtCodigo2");
+	const _txtBebida = document.getElementById("txtBebida");
+	const _txtPrecio2 = document.getElementById("txtPrecio2");
 
-            const _td1 = document.createElement("td");
-            _td1.innerHTML = _txtBebida.value;
+	//codigo2[e] = _txtCodigo2.value;
+	bebida[e] = _txtBebida.value;
+	precio2[e] = parseInt(_txtPrecio2.value);
+	e++;
+	contador2 = contador2 + 1;
 
-            const _td2 = document.createElement("td");
-            _td2.innerHTML = _txtPrecio2.value;
+	const _td0 = document.createElement("td");
+	_td0.innerHTML = contador2;
 
-            const _tr = document.createElement("tr");
-            _tr.appendChild(_td0);
-            _tr.appendChild(_td1);
-            _tr.appendChild(_td2);
+	const _td1 = document.createElement("td");
+	_td1.innerHTML = _txtBebida.value;
 
-            const _tbody2 = document.getElementById("tbody2");
-            _tbody2.appendChild(_tr);
+	const _td2 = document.createElement("td");
+	_td2.innerHTML = _txtPrecio2.value;
 
-		_txtCodigo2.value="";
-		_txtBebida.value="";
-		_txtPrecio2.value="";
-		_txtCodigo2.focus();
+	const _tr = document.createElement("tr");
+	_tr.appendChild(_td0);
+	_tr.appendChild(_td1);
+	_tr.appendChild(_td2);
+
+	const _tbody2 = document.getElementById("tbody2");
+	_tbody2.appendChild(_tr);
+
+	
+	_txtBebida.value = "";
+	_txtPrecio2.value = "";
+	_txtBebida.focus();
 
 
 }
